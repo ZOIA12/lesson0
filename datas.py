a4e11514d062123f46903c57445ce5c2920f19e2
ZOIA12 <varia2019@gmail.com>
2024-09-07 17:09:30 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,18 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>=9.98 and 1000!=1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(6<=8)
+print(6==8)
+
+#4th program
+print('123.456')
+print(float(123.456)*10)
+print(int(1234.56))
+print(1234%10)
+print('123.456' and float(123.456)*10 and int(1234.56) and 1234%10)
